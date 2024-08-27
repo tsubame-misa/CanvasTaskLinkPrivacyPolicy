d0033fe5bdf6c243a43795874a7078c924d5275e
Misato Watanabe <watamisa080513@gmail.com>
2024-08-28 00:53:54 +0900

    init

---
+++ src/app.jsx
@@ -0,0 +1,90 @@
+export function App() {
+  return (
+    <>
+      <header className="hero is-primary is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Canvas Task Link Privacy Policy</h1>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className="section">
+          <div className="container">
+            <h1 className="title is-3">Chrome拡張機能のプライバシーポリシー</h1>
+            <div className="content">
+              本プライバシーポリシーは、tsubame33（以下、「当開発者」）が開発したGoogle
+              Chromeの拡張機能「Canvas Task
+              Link」（以下、「拡張機能」とします）を利用する際に、利用者の個人情報またはそれに準ずる情報を取り扱う際に、当開発者が遵守する方針を示したものです。
+            </div>
+            <h2 className="title is-4">基本方針</h2>
+            <div className="content">
+              当開発者は、個人情報の重要性を深く認識し、その保護を社会的責務と考えています。法令を遵守し、拡張機能において取り扱う個人情報の取得、利用、管理を適正に行います。
+            </div>
+
+            <h2 className="title is-4">適用範囲</h2>
+            <div className="content">
+              本プライバシーポリシーは、当開発者が開発した拡張機能にのみ適用されます。
+            </div>
+
+            <h2 className="title is-4">個人情報の取得と利用目的</h2>
+            <div className="content">
+              当開発者は、拡張機能を通じて次の個人情報を収集し、利用します。
+            </div>
+
+            <h3 className="title is-5">取得項目：取得方法</h3>
+            <div className="content">
+              <ul>
+                <li>
+                  CanvasLMSの課題：CavasLMSにログイン後APIを通して取得されます。
+                </li>
+                <li>
+                  当拡張機能がGoogleカレンダーに登録したイベント：ユーザーがGoogleカレンダーの連携を認証後APIを通して取得されます。
+                </li>
+              </ul>
+            </div>
+
+            <h3 className="title is-5">利用目的</h3>
+            <div className="content">
+              収集した情報は、拡張機能のサービスの提供にのみ使用されます。
+            </div>
+
+            <h3 className="title is-5">保存期間について</h3>
+            <div className="content">
+              拡張機能では、個人情報の保存は致しません
+            </div>
+
+            <h2 className="title is-4">個人情報の管理</h2>
+            <div className="content">
+              当開発者は、拡張機能において収集された個人情報を適切に管理し、以下の取り組みを行います。
+            </div>
+
+            <h3 className="title is-5">安全管理措置</h3>
+            <div className="content">
+              個人情報が漏洩、滅失しないよう、拡張機能内での情報の取扱いには最新のセキュリティ対策を講じます。また、収集した情報を外部のサーバーに送信することはありません。
+            </div>
+
+            <h3 className="title is-5">個人情報の第三者への提供</h3>
+            <div className="content">
+              当開発者は、ユーザーの同意を得ることなく、収集した個人情報を第三者に提供することはありません。今後、第三者提供を行う場合は、事前にユーザーに情報を提供し、同意を得た場合にのみ行います。
+            </div>
+
+            <h3 className="title is-5">問い合わせ先</h3>
+            <div className="content">
+              <div>
+                拡張機能や個人情報の取扱いに関して質問がある場合は、以下の連絡先までお問い合わせください。
+              </div>
+
+              <div className="pt-3">
+                メールアドレス：watamisa080513@gmail.com
+              </div>
+            </div>
+
+            <h2 className="title is-4">策定日</h2>
+            <div>令和6年8月26日 策定</div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
